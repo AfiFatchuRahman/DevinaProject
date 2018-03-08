25bcd5b5db25bcfd96d5d693325c99df744d702d
zackpermana <zackpermana@gmail.com>
2018-03-08 19:21:05 +0700

    pemba

--- Assets/Scripts/Pembagian/NumberTen.cs
+++ Assets/Scripts/Pembagian/NumberTen.cs
@@ -24,7 +24,61 @@ public class NumberTen : MonoBehaviour {
 
 	IEnumerator PlayAnimasi()
 	{
+		
 		yield return new WaitForSeconds (1f);
-		candies [0].transform.DOMove (new Vector3(-1f,-1f,0f),1f);
+		candies [0].transform.DOMove (new Vector3(-1.3f + 0.2f,-0.9f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [1].transform.DOMove (new Vector3(-1.3f + 2.1f+ 0.2f,-0.90f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [2].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 0.2f,-0.90f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [3].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 2.1f + 0.2f,-0.90f,0f),1f);
+
+		yield return new WaitForSeconds (1f);
+		candies [4].transform.DOMove (new Vector3(-1.3f + 0.2f+0.3f,-0.9f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [5].transform.DOMove (new Vector3(-1.3f + 2.1f+ 0.2f+0.3f,-0.90f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [6].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 0.2f+0.3f,-0.90f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [7].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 2.1f + 0.2f+0.3f,-0.90f,0f),1f);
+
+		yield return new WaitForSeconds (1f);
+		candies [8].transform.DOMove (new Vector3(-1.3f + 0.2f+0.3f+0.3f,-0.9f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [9].transform.DOMove (new Vector3(-1.3f + 2.1f+ 0.2f+0.3f+0.3f,-0.90f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [10].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 0.2f+0.3f+0.3f,-0.90f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [11].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 2.1f + 0.2f+0.3f+0.3f,-0.90f,0f),1f);
+
+		yield return new WaitForSeconds (1f);
+		candies [12].transform.DOMove (new Vector3(-1.3f + 0.2f,-0.9f+ 0.2f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [13].transform.DOMove (new Vector3(-1.3f + 2.1f+ 0.2f,-0.90f+ 0.2f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [14].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 0.2f,-0.90f+ 0.2f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [15].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 2.1f + 0.2f,-0.90f+ 0.2f,0f),1f);
+
+		yield return new WaitForSeconds (1f);
+		candies [16].transform.DOMove (new Vector3(-1.3f + 0.2f+0.3f,-0.9f+ 0.2f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [17].transform.DOMove (new Vector3(-1.3f + 2.1f+ 0.2f+0.3f,-0.90f+ 0.2f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [18].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 0.2f+0.3f,-0.90f+ 0.2f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [19].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 2.1f + 0.2f+0.3f,-0.90f+ 0.2f,0f),1f);
+
+		yield return new WaitForSeconds (1f);
+		candies [20].transform.DOMove (new Vector3(-1.3f + 0.2f+0.3f+0.3f,-0.9f+ 0.2f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [21].transform.DOMove (new Vector3(-1.3f + 2.1f+ 0.2f+0.3f+0.3f,-0.90f+ 0.2f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [22].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 0.2f+0.3f+0.3f,-0.90f+ 0.2f,0f),1f);
+		yield return new WaitForSeconds (1f);
+		candies [23].transform.DOMove (new Vector3(-1.3f + 2.1f + 2.1f + 2.1f + 0.2f+0.3f+0.3f,-0.90f+ 0.2f,0f),1f);
+
+
 	}
 }
